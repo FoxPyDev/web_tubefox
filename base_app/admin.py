6a8204bac51a60014ca4e95e03ec411ed64daee3
FoxPyDev <foxpythondev@gmail.com>
2024-03-06 16:04:46 +0200

    In the process of studying the framework

---
+++ base_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from base_app.models import Workers
+
+admin.site.register(Workers)
---
+++ base_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from base_app.models import Workers
+from django.http import HttpResponse
+
+def index_page(request):
+
+    all_workers = Workers.objects.all()
+
+    return render(request, 'index.html', {'data': all_workers})
