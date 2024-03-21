dc0071d652183e9489b82b20500328766f999cf6
FoxPyDev <foxpythondev@gmail.com>
2024-03-21 23:42:39 +0200

    basic functionality prototyping

--- base_app/views.py
+++ base_app/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from base_app.models import Workers
 from django.http import HttpResponse
+from tubefox import TubeFox
 
 
 def index_page(request):
-    all_workers = Workers.objects.all()
-    return render(request, 'index.html', {'data': all_workers})
+    if request.method == "POST":
+        data = request.POST
+        yt_object = TubeFox(data['yt_link'])
+        return render(request, 'index.html',
+                      {'title': yt_object.title,
+                       'description': yt_object.description,
+                       'keywords': yt_object.keywords,
+                       'download_video': yt_object.app_collected_data.collect_video_links()
+                       [max(yt_object.app_collected_data.collect_video_links().keys())],
+                       'download_thumbnail': yt_object.web_collected_data.collect_thumbnail_links()
+                       [max(yt_object.web_collected_data.collect_thumbnail_links().keys())],
+                       'download_audio': yt_object.app_collected_data.collect_audio_links()
+                       [max(yt_object.app_collected_data.collect_audio_links().keys())],
+                       }
+                      )
+    elif request.method == "GET":
+        return render(request, 'index.html')
+
