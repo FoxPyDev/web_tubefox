bb252f5c74db60111b90d210b4e41ebd676876b0
FoxPyDev <foxpythondev@gmail.com>
2024-05-22 23:15:34 +0300

    frontend prototyping

--- base_app/views.py
+++ base_app/views.py
@@ -27,8 +27,31 @@ def index_page(request):
         return render(request, 'index.html')
 
 
+# def bootstrap_page(request):
+#     return render(request, 'bootstrap.html')
+
+
 def bootstrap_page(request):
-    return render(request, 'bootstrap.html')
+    if request.method == "POST":
+        data = request.POST
+        try:
+            yt_object = TubeFox(data['yt_link'])
+            return render(request, 'bootstrap.html',
+                          {'title': yt_object.title,
+                           'description': yt_object.description,
+                           'keywords': yt_object.keywords,
+                           'download_video': yt_object.app_collected_data.collect_video_links()
+                           [max(yt_object.app_collected_data.collect_video_links().keys())],
+                           'download_thumbnail': yt_object.web_collected_data.collect_thumbnail_links()
+                           [max(yt_object.web_collected_data.collect_thumbnail_links().keys())],
+                           'download_audio': yt_object.app_collected_data.collect_audio_links()
+                           [max(yt_object.app_collected_data.collect_audio_links().keys())],
+                           }
+                          )
+        except:
+            return render(request, 'bootstrap.html')
+    elif request.method == "GET":
+        return render(request, 'bootstrap.html')
 
 
 def download_video(request):
