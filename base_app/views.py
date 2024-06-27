59831d97b43987b3ce1bd5a7e3936f8f425ffb00
FoxPyDev <foxpythondev@gmail.com>
2024-06-27 14:23:39 +0300

    Download Speed Up. Change audio download method/

--- base_app/views.py
+++ base_app/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 import requests
 from tubefox import TubeFox
 from tubefox.yt_app_version_updater import get_yt_app_latest_version
 from tubefox.subtitles import Subtitles
 from django.http import StreamingHttpResponse
-import moviepy.editor as mp
-from io import BytesIO
-import tempfile
 
 
 def index_page(request):
@@ -21,7 +18,7 @@ def index_page(request):
                            'download_video': yt_object.app_collected_data.collect_video_links()
                            [max(yt_object.app_collected_data.collect_video_links().keys())],
                            'download_muted_video': yt_object.app_collected_data.collect_muted_video_links()
-                           [max(yt_object.app_collected_data.collect_muted_video_links().keys())],
+                           [max(yt_object.app_collected_data.collect_muted_video_links().keys())]['url'],
                            'download_thumbnail': yt_object.web_collected_data.collect_thumbnail_links()
                            [max(yt_object.web_collected_data.collect_thumbnail_links().keys())],
                            'download_audio': yt_object.app_collected_data.collect_audio_links()
@@ -47,6 +44,8 @@ def bootstrap_page(request):
                            [max(yt_object.app_collected_data.collect_muted_video_links().keys())],
                            'download_thumbnail': yt_object.web_collected_data.collect_thumbnail_links()
                            [max(yt_object.web_collected_data.collect_thumbnail_links().keys())],
+                           'download_audio': yt_object.app_collected_data.collect_audio_links()
+                           [max(yt_object.app_collected_data.collect_audio_links().keys())],
                            'status': 'ok'
                            }
                           )
@@ -56,49 +55,103 @@ def bootstrap_page(request):
     elif request.method == "GET":
         return render(request, 'bootstrap.html')
 
-
 def download_video(request):
     if request.method == "POST":
         video_url = request.POST.get('download_video')
-        headers = {
-            'Content-Type': 'application/json',
-            'User-Agent': f'com.google.android.youtube/{get_yt_app_latest_version()} (Linux; U; Android 12; GB) gzip'
-        }
-        video_response = requests.get(video_url, stream=True, headers=headers)
+        print(video_url)
 
-        if video_response.status_code == 200 and 'Content-Length' in video_response.headers:
-            content_length = int(video_response.headers['Content-Length'])
-            if content_length > 0:
-                response = StreamingHttpResponse(
-                    (chunk for chunk in video_response.iter_content(chunk_size=8192)),
-                    content_type='video/mp4'
-                )
-                response['Content-Disposition'] = 'attachment; filename="video.mp4"'
-                return response
+        # Obtain the total size of the video stream using a HEAD request
+        response = requests.head(video_url)
+        if response.status_code == 200:
+            total_size = int(response.headers.get('Content-Length', 0))
 
-        return HttpResponse("Reload page and try again")
+            chunk_size = 10 * 1024 * 1024  # Chunk size in bytes (10 MB)
+            bytes_downloaded = 0  # Counter for downloaded bytes
 
+            def stream_video():
+                nonlocal bytes_downloaded
+                retries = 5  # Number of retries for each chunk
+                while bytes_downloaded < total_size:
+                    start = bytes_downloaded
+                    end = min(bytes_downloaded + chunk_size - 1, total_size - 1)
+                    headers = {'Range': f'bytes={start}-{end}'}
+                    success = False
+
+                    for attempt in range(retries):
+                        response = requests.get(video_url, headers=headers, stream=True)
+                        if response.status_code == 206:
+                            success = True
+                            break
+                        else:
+                            print(f"Attempt {attempt + 1} failed with status code {response.status_code}")
+
+                    if not success:
+                        print("Failed to download the chunk after several attempts")
+                        return
+
+                    for chunk in response.iter_content(chunk_size=4096):
+                        yield chunk
+
+                    bytes_downloaded += end - start + 1
+                    print(f"Downloaded {bytes_downloaded} bytes")
+
+            response = StreamingHttpResponse(stream_video(), content_type='video/mp4')
+            response['Content-Disposition'] = 'attachment; filename="output.mp4"'
+            response['Content-Length'] = str(total_size)
+
+            return response
+        else:
+            return HttpResponseRedirect('/bootstrap')
+    else:
+        return HttpResponseRedirect('/bootstrap')
+
+# def download_muted_video(request):
+#     if request.method == "POST":
+#         video_url = request.POST.get('download_muted_video')
+#         print(video_url)
+#         headers = {
+#             'Content-Type': 'application/json',
+#             'User-Agent': f'com.google.android.youtube/{get_yt_app_latest_version()} (Linux; U; Android 12; GB) gzip'
+#         }
+#         video_response = requests.get(video_url, stream=True, headers=headers)
+#
+#         if video_response.status_code == 200 and 'Content-Length' in video_response.headers:
+#             content_length = int(video_response.headers['Content-Length'])
+#             if content_length > 0:
+#                 response = StreamingHttpResponse(
+#                     (chunk for chunk in video_response.iter_content(chunk_size=8192)),
+#                     content_type='video/mp4'
+#                 )
+#                 response['Content-Disposition'] = 'attachment; filename="muted_video.mp4"'
+#                 return response
+#
+#         return HttpResponse("Reload page and try again")
 
 def download_muted_video(request):
     if request.method == "POST":
-        video_url = request.POST.get('download_muted_video')
-        print(video_url)
-        headers = {
-            'Content-Type': 'application/json',
-            'User-Agent': f'com.google.android.youtube/{get_yt_app_latest_version()} (Linux; U; Android 12; GB) gzip'
-        }
-        video_response = requests.get(video_url, stream=True, headers=headers)
+        # Отримуємо заголовок Content-Length, щоб дізнатися загальний розмір стріму
+        response = requests.head('download_muted_video')
+        total_size = int(response.headers.get('Content-Length', 0))
 
-        if video_response.status_code == 200 and 'Content-Length' in video_response.headers:
-            content_length = int(video_response.headers['Content-Length'])
-            if content_length > 0:
-                response = StreamingHttpResponse(
-                    (chunk for chunk in video_response.iter_content(chunk_size=8192)),
-                    content_type='video/mp4'
-                )
-                response['Content-Disposition'] = 'attachment; filename="muted_video.mp4"'
-                return response
+        chunk_size = 10 * 1024 * 1024  # Розмір частини у байтах (тут 10 МБ)
+        bytes_downloaded = 0  # Лічильник завантажених байт
 
+        # Запити частин стріму і запис у файл 'output.mp4'
+        with open(f'output.webm', 'wb') as f:
+            while bytes_downloaded < total_size:
+                start = bytes_downloaded
+                end = min(bytes_downloaded + chunk_size - 1, total_size - 1)
+                headers = {'Range': f'bytes={start}-{end}'}
+                response = requests.get('download_muted_video', headers=headers, stream=True)
+
+                for chunk in response.iter_content(chunk_size=4096):
+                    f.write(chunk)
+
+                bytes_downloaded += end - start + 1
+                print(f"Downloaded {bytes_downloaded} bytes")
+
+        return HttpResponse("Download complete. Check 'output.mp4' file.")
+    else:
         return HttpResponse("Reload page and try again")
 
 
@@ -212,6 +265,8 @@ def audio_page(request):
                            [max(yt_object.web_collected_data.collect_thumbnail_links().keys())],
                            'download_video': yt_object.app_collected_data.collect_video_links()
                            [max(yt_object.app_collected_data.collect_video_links().keys())],
+                           'download_audio': yt_object.app_collected_data.collect_audio_links()
+                           [max(yt_object.app_collected_data.collect_audio_links().keys())],
                            'status': 'ok'}
                           )
         except:
@@ -223,34 +278,43 @@ def audio_page(request):
 
 def download_audio(request):
     if request.method == "POST":
-        video_url = request.POST.get('download_video')
-        headers = {
-            'Content-Type': 'application/json',
-            'User-Agent': f'com.google.android.youtube/{get_yt_app_latest_version()} (Linux; U; Android 12; GB) gzip'
-        }
-        video_response = requests.get(video_url, stream=True, headers=headers)
+        audio_url = request.POST.get('download_audio')
+        print(audio_url)
 
-        if video_response.status_code == 200 and 'Content-Length' in video_response.headers:
-            content_length = int(video_response.headers['Content-Length'])
-            if content_length > 0:
-                _, temp_video_path = tempfile.mkstemp(suffix='.mp4')
-                with open(temp_video_path, 'wb') as temp_video:
-                    temp_video.write(video_response.content)
+        try:
+            # Obtain the total size of the audio stream using a HEAD request
+            response = requests.head(audio_url)
+            if response.status_code == 200:
+                total_size = int(response.headers.get('Content-Length', 0))
 
-                video = mp.VideoFileClip(temp_video_path)
+                chunk_size = 10 * 1024 * 1024  # Chunk size in bytes (10 MB)
+                bytes_downloaded = 0  # Counter for downloaded bytes
 
-                # Вибираємо аудіодоріжку з відео
-                audio = video.audio
+                def stream_audio():
+                    nonlocal bytes_downloaded
+                    while bytes_downloaded < total_size:
+                        start = bytes_downloaded
+                        end = min(bytes_downloaded + chunk_size - 1, total_size - 1)
+                        headers = {'Range': f'bytes={start}-{end}'}
+                        response = requests.get(audio_url, headers=headers, stream=True)
+                        print(response.status_code)
+                        for chunk in response.iter_content(chunk_size=4096):
+                            yield chunk
 
-                # Зберігаємо аудіо у форматі mp3
-                _, temp_audio_path = tempfile.mkstemp(suffix='.mp3')
-                audio.write_audiofile(temp_audio_path, bitrate='320k')
+                        bytes_downloaded += end - start + 1
+                        print(f"Downloaded {bytes_downloaded} bytes")
 
-                with open(temp_audio_path, 'rb') as f:
-                    audio_data = f.read()
+                response = StreamingHttpResponse(stream_audio(), content_type='audio/opus')
+                response['Content-Disposition'] = 'attachment; filename="audio_output.opus"'
+                response['Content-Length'] = str(total_size)
 
-                response = HttpResponse(audio_data, content_type='audio/mpeg')
-                response['Content-Disposition'] = f'attachment; filename="audio.mp3"'
                 return response
+            else:
+                return HttpResponseRedirect('/audio')  # Redirect if HEAD request fails
+        except Exception as e:
+            print(f"Error downloading audio: {e}")
+            return HttpResponseRedirect('/audio')  # Redirect on error
+    else:
+        return HttpResponseRedirect('/audio')  # Redirect if not POST request
 
-        return HttpResponse("Reload page and try again")
+
